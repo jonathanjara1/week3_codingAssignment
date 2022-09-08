f0bed2d930a6592f295324134654b638dc80f95a
jonathanjara1 <jonathanjaramillo45@hotmail.com>
2022-09-08 14:38:24 -0400

    test

--- week3.js
+++ week3.js
@@ -1,70 +1,152 @@
-// CODING STEPS
 
-//1
-
-// let ages = [3,9,23,64,2,8,28,93];
-// let numbers = ages.map(function(element){
-//     return element.age
-// });
-
-// console.log(ages);
-
-// let firstElement = ages[ages.length - ages.lenght];
-// let lastElement = ages[ages.lenght-1];
-
-// let answer = lastElement - firstElement
-
-// console.log(lastElement);
-// console.log(firstElement);
-// console.log(answer);
-
-// 2
-
-// const array = [3,9,23,64,2,8,28,93];
-
-// const firstElement = array[array.lenght - array.length];
-// const lastElement = array[array.lenght-1];
-
-// const answer = lastElement - firstElement;
-
-// console.log(lastElement);
-// console.log(firstElement);
-// console.log(answer);
 
 //1
 
 
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
-let result = ((ages[ages.lenght -1]) - ages[0]);
+console.log(ages);
+
+ages.push(10);
+
+let result = ((ages[ages.length -1]) - ages[0]);
 
 console.log(result);
 
+let total1 = 0;
+for(let age of ages) {
+    total1 += age;
+}
+total1 /= ages.length;
+console.log(total1);
+
+
 //2
-
-
-
 
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
 
-let total = 0;
+console.log(names);
+ console.log("Lenght of names array:", names.length);
+// a.
+
+let lettersInNames = 0;
 
 for(let i = 0; i < names.length; i++){
-    total = total + names[i];
+console.log("Index #: ",i, "name", names[i]);
+console.log("Index #: ",i, "count", names[i].length);
+lettersInNames = lettersInNames + names[i].length;
 }
+
+console.log("Sum of letters in names array", lettersInNames); 
+let average = lettersInNames / names.length;
+console.log("average letters in names array", average);
+
+//b.
+
+console.log(names);
+let concatenatedNames = "";
+console.log(concatenatedNames)
+
+for(let i = 0; i < names.length; i++) {
+    console.log("each name: ", names[i]);
+    concatenatedNames = concatenatedNames + names[i] + " ";
+}
+
+console.log("Concat names: ",concatenatedNames)
+
+
+3// Using the array lenght property
+
+4// Using an index, and the first value is accessed unsing index 0
+
+
+
+//5 
+
+let nameLenghts = [];
+console.log(nameLenghts)
+
+for(let i = 0; i < names.length; i++) {
+   console.log("name", names[i]);
+   console.log("letter count", names[i].length);
+   nameLenghts.push(names[i].length);
+}
+
+console.log("Pushed sum of letters of each name to nameLenghts",nameLenghts);
+
+
+
+// 6
+
+let total = 0;
+
+for(let i = 0; i < nameLenghts.length; i++){
+    console.log("nameLenghts", nameLenghts[i]);
+    total = total + nameLenghts[i];
+}
+
+console.log("sum of namelenghts array", total);
+
+
+// 7
+
+
+
+
+// 8
+
+function fullName(firstName, lastName){
+    return firstName + " " + lastName;
+}
+
+console.log(fullName("Jonathan", "Jaramillo"));
+
+
+
+// 9 
+
+let myNumbers = [100, 200, 300, 400];
+function sumArray(array) {
     
-let average = total / names.length;
-console.log(average);
- 
+    
+
+    let sumOfNumbers = 0;
+
+    for(let i = 0; i < array.length; i++) {
+        console.log ("printing all numbers of array", array[i]);
+        sumOfNumbers = sumOfNumbers + array[i];
+    }
+
+    console.log("sum of all numbers in array", sumOfNumbers);
+
+    if(sumOfNumbers > 100){
+        return true;
+    
+    } else {
+        return false;
+    }
 
 
-//5
+}
+
+console.log(sumArray(myNumbers));
 
 
-let namesArray = ['Kelly', 'Sam', 'Kate'];
+//10
 
-let nameLengths = namesArray.map(function(x){
-    return x.nameLenghts;
-});
+let numberMyNumbers = [1, 2, 3, 4, 5];
 
-console.log(nameLengths);
+function findAverage(arr1){
+    let average1 = 0;
+    for(let i = 0; i < arr1.length; i++){
+        let currentNum = arr1[i];
+        average1 += currentNum;
+    }
+    average1 = average1 / arr1.length;
+    return average1
+}
+
+console.log(findAverage(numberMyNumbers));
+
+//12
+
